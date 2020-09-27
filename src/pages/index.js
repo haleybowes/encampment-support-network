e8f448897ea7543f6049bfe19eae8954d3651b18
Haley Bowes (LCL) <Haley.Bowes@loblaw.ca>
2020-09-26 21:14:45 -0400

    update prettier config, get contentful fetching working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const Home = () => {
-	return <h1>hi!</h1>
+const Home = ({ data }) => {
+	console.log(data);
+	return <h1>hi!</h1>;
 };
+
+export const query = graphql`
+	query MyQuery {
+		contentfulPage(internalName: { eq: "Main" }) {
+			id
+			content {
+				id
+				body {
+					json
+				}
+			}
+		}
+	}
+`;
 
 export default Home;
