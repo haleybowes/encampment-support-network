90bfbe1fd7dc8d6c7b743991b63a760a5df45de2
Haley Bowes (LCL) <Haley.Bowes@loblaw.ca>
2020-10-19 09:02:47 -0400

    add link wrapper

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,7 +5,58 @@ import { BLOCKS } from '@contentful/rich-text-types';
 
 import RichText from '../components/RichText';
 
-const Image = styled.img``;
+const StyledLink = styled.div`
+	a {
+		color: #2f56b0;
+		position: relative;
+		text-decoration: none;
+		transition: color 0.4s ease-out;
+	}
+
+	a:hover {
+		right: 0;
+		text-decoration: none;
+	}
+
+	a:hover:after {
+		border-color: #457dfb;
+		right: 0;
+	}
+
+	a:after {
+		border-radius: 1em;
+		border-top: 0.1em solid #2f56b0;
+		content: '';
+		position: absolute;
+		right: 100%;
+		bottom: 0.14em;
+		left: 0;
+		transition: right 0.3s cubic-bezier(0, 0.5, 0, 1), border-color 0.4s ease-out;
+	}
+
+	a:hover:after {
+		animation: anchor-underline 2s cubic-bezier(0, 0.5, 0, 1);
+		border-color: #457dfb;
+	}
+
+	@keyframes anchor-underline {
+		0%,
+		10% {
+			left: 0;
+			right: 100%;
+		}
+		40%,
+		60% {
+			left: 0;
+			right: 0;
+		}
+		90%,
+		100% {
+			left: 100%;
+			right: 0;
+		}
+	}
+`;
 
 const Link = ({ link: { linkUrl, linkText, image } }) => {
 	const isInternal = /^\/(?!\/)/.test(linkUrl);
@@ -18,18 +69,24 @@ const Link = ({ link: { linkUrl, linkText, image } }) => {
 	const children = (
 		<>
 			{linkText && <RichText document={linkText.json} customOptions={options} />}
-			{image && <Image src={image.file.url} alt="" />}
+			{image && <img src={image.file.url} alt="" />}
 		</>
 	);
 
 	if (isInternal) {
-		return <GatsbyLink to={linkUrl}>{children}</GatsbyLink>;
+		return (
+			<StyledLink>
+				<GatsbyLink to={linkUrl}>{children}</GatsbyLink>
+			</StyledLink>
+		);
 	}
 
 	return (
-		<a href={linkUrl} rel="noreferrer" target="_blank">
-			{children}
-		</a>
+		<StyledLink>
+			<a href={linkUrl} rel="noreferrer" target="_blank">
+				{children}
+			</a>
+		</StyledLink>
 	);
 };
 
